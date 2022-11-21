173b3299640bfc82e2a185388a5323c2a4e2652b
sufailbadushat <sufailbadushat@gamil.com>
2022-11-21 12:11:39 +0530

    Bug fix branch

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        double result;
         Scanner sc = new Scanner(System.in);
-        System.out.println("/*................Calculator Menu................*/");
-        System.out.println("Press 1 for addition");
-        System.out.println("Press 2 for subtraction");
-        System.out.println("Press 3 for Multiplication");
-        System.out.println("Press 4 for Division");
-        System.out.println("Press 5 for finding Square");
 
-        System.out.println("Enter your choice:");
-        int choice = sc.nextInt();
+        while (true){
+            System.out.println("/*................Calculator Menu................*/");
+            System.out.println("Press 1 for addition");
+            System.out.println("Press 2 for subtraction");
+            System.out.println("Press 3 for Multiplication");
+            System.out.println("Press 4 for Division");
+            System.out.println("Press 5 for finding Square");
+            System.out.println("Press 6 for exit");
 
-        System.out.println("Enter two numbers");
-        double number1 = sc.nextDouble();
-        double number2 = sc.nextDouble();
+            System.out.println("Enter your choice:");
+            int choice = sc.nextInt();
 
-        switch (choice){
-            case 1:
-                double result = number1+number2;
-                System.out.println("The answer is ");
-                System.out.println(result);
-                break;
-            case 2:
-                result = number1-number2;
-                System.out.println("The answer is ");
-                System.out.println(result);
-                break;
-            case 3:
-                result = number1*number2;
-                System.out.println("The answer is ");
-                System.out.println(result);
-                break;
-            case 4:
-                result = number1/number2;
-                System.out.println("The answer is ");
-                System.out.println(result);
-                break;
-            case 5:
-                double sqrt1 = number1*number1;
-                double sqrt2 = number2*number2;
-                System.out.println("The Square root of "+ number1 + " is "+ sqrt1);
-                System.out.println("The Square root of "+ number2 + " is "+ sqrt2);
-                break;
+            System.out.println("Enter two numbers");
+            double number1 = sc.nextDouble();
+            double number2 = sc.nextDouble();
+
+
+            switch (choice){
+                case 1:
+                    result = number1+number2;
+                    System.out.println("The answer is ");
+                    System.out.println(result);
+                    break;
+                case 2:
+                    result = number1-number2;
+                    System.out.println("The answer is ");
+                    System.out.println(result);
+                    break;
+                case 3:
+                    result = number1*number2;
+                    System.out.println("The answer is ");
+                    System.out.println(result);
+                    break;
+                case 4:
+                    result = number1/number2;
+                    System.out.println("The answer is ");
+                    System.out.println(result);
+                    break;
+                case 5:
+                    double sqrt1 = number1*number1;
+                    double sqrt2 = number2*number2;
+                    System.out.println("The Square root of "+ number1 + " is "+ sqrt1);
+                    System.out.println("The Square root of "+ number2 + " is "+ sqrt2);
+                    break;
+                case 6:
+                    System.exit(0);
+            }
+
         }
-
     }
 }
